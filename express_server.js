d95624675a037f46eed826b1e9da0230c247608d
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-23 16:49:13 -0500

    init basic setup

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express(); // instance of express class -> returns your application framework
+const PORT = 8000
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
+app.get('/', (req, res) => { // handle the response from the callback from the get method of the app obj // first arg is the page requested -> url/PATH -> '/' is the root of path
+  res.send('<h1>Hey Folks</h1>')
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port: ${PORT}`);
+})
