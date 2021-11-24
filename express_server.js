6db5913c3b9b14e2dae96101ecf609631add1240
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-24 15:52:56 -0500

    add route handler for the param /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -2,38 +2,40 @@ const express = require('express');
 const app = express(); // instance of express class -> returns your application framework
 const PORT = 8080;
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+const { URL_DATABASE } = require('./constants')
 
 app.set('view engine', 'ejs');
 
 // URLs index page
+// ====================================================
 app.get('/urls', (req, res) => { // handles the response with a callback of the get method of the app obj // first arg is the page requested -> url/PATH -> '/' is the root of path
-
-  
+  const templateVars = {
+    urls: URL_DATABASE
+  }
   // views, pass variables
-  res.render('urls_index', {
-    urls: urlDatabase
-  });
+  res.render('urls_index', templateVars);
 });
 
-//about page
-// app.get('/about', (req, res) => {
-//   res.render("pages/about"); // views
-// });
+// Route Param :shortURL <-- setting the param
+// ====================================================
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: URL_DATABASE[req.params.shortURL],
+  }
+  res.render('urls_show', templateVars)
+})
 
 // API route handler -> JSON res
+// ====================================================
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase); // sends our existing object as a json file format // which can then be parsed by the client
 });
 
 
-
 // get the server listening as soon as possible
+// ====================================================
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}`);
 });
 
-// ====================================================
