c1b2eb0f3d211c2af99beea51577d79c8785946c
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-25 18:27:27 -0500

    handle post request from the delete button from index template

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 
-/** TODO: 
+/** TODO:
  * [] handle unknown short url requests
- * 
+ *
 */
 const express = require('express');
 const morgan = require('morgan');
@@ -41,40 +41,57 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+// FIXME: the urls conditional can be more truthy
 // Post Route <-- listening for a post req to /urls
 // ====================================================
 app.post('/urls', (req, res) => {
   const serial = generateRandomString();
   const postInput = `http://${req.body.longURL}`;
   const urlsList = Object.values(URL_DATABASE);
-
+  
   // checks if URL is already in database & update
   if (!urlsList.includes(postInput)) {
     URL_DATABASE[serial] = postInput;
   }
-
+  
   // redirect to /urls/:shortURL
-  res.redirect(`/urls/${serial}`)
+  res.redirect(`/urls/${serial}`);
 });
+
+
+// DELETE POST
+app.post('/urls/:shortURL/delete', (req, res) => {
+
+  delete URL_DATABASE[req.params.shortURL];
+  res.redirect('/urls');
+  
+});
+
 
 // FIXME: needs to handle unknown shortURL
 // Redirect to longURL - shortened for internal purposes -> out in the wild
 // ====================================================
 app.get('/u/:shortURL', (req, res) => {
   const longURL = URL_DATABASE[req.params.shortURL];
-  console.log(longURL);
-  res.redirect(longURL)
-})
+
+  res.redirect(longURL);
+});
 
 // Route Param :shortURL <-- setting the param | SHOWs the current tinyURL from the param gievn by browser
 // ====================================================
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: URL_DATABASE[req.params.shortURL],
-  };
+    const urls = Object.keys(URL_DATABASE)
+    const templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: URL_DATABASE[req.params.shortURL],
+    };
+    if (!urls.includes(templateVars.shortURL)){
+      return res.redirect('/urls')
+    }
   res.render('urls_show', templateVars);
 });
+
+
 
 // API route handler -> JSON res
 // ====================================================
@@ -82,9 +99,11 @@ app.get('/urls.json', (req, res) => {
   res.json(URL_DATABASE); // sends our existing object as a json file format // which can then be parsed by the client
 });
 
+
+
+
 // get the server listening as soon as possible
 // ====================================================
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}`);
 });
-
